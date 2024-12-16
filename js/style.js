f8dce0a04af4cf7b7e45d688ad81e6cd6a35602a
sudiptodas153 <sudiptodas4093@gmail.com>
2024-12-16 14:33:38 +0600

    functionality recover

--- js/style.js
+++ js/style.js
@@ -15,8 +15,40 @@ document.getElementById('donate').addEventListener('click', function (event) {
         const balance = document.getElementById('account').innerText;
         const balanceNumber = parseFloat(balance);
 
+
+
         const result = balanceNumber - addMoneyNumber;
+
         document.getElementById('account').innerText = result;
+
+
+
+        const historyItem = document.createElement('div')
+        historyItem.className = 'bg-white p-4 rounded-md border-2 border-gray-200';
+
+        historyItem.innerHTML = `
+        
+<p class="text-xl font-bold mb-4">${addMoneyNumber} Taka is Donated for flood at Noakhali, Bangladesh</p>
+<p>Date: ${new Date().toLocaleDateString()}</p>
+
+
+
+        `;
+
+
+
+        const historyContainer = document.getElementById('added')
+
+        historyContainer.appendChild(historyItem);
+
+
+
+        alert('Donate Successful')
+
+
+
+
+
 
     }
 
@@ -45,8 +77,38 @@ document.getElementById('donate2').addEventListener('click', function (event) {
         const balance = document.getElementById('account').innerText;
         const balanceNumber = parseFloat(balance);
 
+
+
         const result = balanceNumber - addMoneyNumber;
+
+
         document.getElementById('account').innerText = result;
+
+
+
+
+
+
+        const historyItem = document.createElement('div')
+        historyItem.className = 'bg-white p-4 rounded-md border-2 border-gray-200';
+
+        historyItem.innerHTML = `
+        
+<p class="text-xl mb-4 font-bold">${addMoneyNumber} Taka is Donated for Flood Relief in Feni, Bangladesh</p>
+
+<p>Date: ${new Date().toLocaleDateString()}</p>
+
+
+        `;
+
+
+
+        const historyContainer = document.getElementById('added')
+
+        historyContainer.appendChild(historyItem)
+
+
+        alert('Donate Successful')
 
     }
 
@@ -68,7 +130,7 @@ document.getElementById('donate3').addEventListener('click', function (event) {
 
     if (!isNaN(addMoney) && addMoney > 0) {
 
-        
+
 
 
         const balance2 = document.getElementById('first3').innerText;
@@ -83,11 +145,35 @@ document.getElementById('donate3').addEventListener('click', function (event) {
         const balance = document.getElementById('account').innerText;
         const balanceNumber = parseFloat(balance);
 
+
         const result = balanceNumber - addMoneyNumber;
+
 
         document.getElementById('account').innerText = result;
 
 
+
+
+        const historyItem = document.createElement('div')
+        historyItem.className = 'bg-white p-4 rounded-md border-2 border-gray-200';
+
+        historyItem.innerHTML = `
+        
+<p class="text-xl mb-4 font-bold">${addMoneyNumber} Taka is Donated for Injured in the Quota Movement, Bangladesh</p>
+<p>Date: ${new Date().toLocaleDateString()}</p>
+
+
+
+        `;
+
+
+
+        const historyContainer = document.getElementById('added')
+
+        historyContainer.appendChild(historyItem)
+
+
+        alert('Donate Successful')
 
     }
 
@@ -99,3 +185,67 @@ document.getElementById('donate3').addEventListener('click', function (event) {
 
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+const historyPart = document.getElementById('history');
+const donationPart = document.getElementById('donation');
+
+historyPart.addEventListener('click', function () {
+
+    historyPart.classList.add('bg-lime-400');
+
+    donationPart.classList.remove('bg-lime-400')
+
+    document.getElementById('newpage').classList.add('hidden')
+
+
+
+
+})
+historyPart.addEventListener('click', function () {
+
+    historyPart.classList.add('bg-lime-400');
+
+    donationPart.classList.remove('bg-lime-400')
+
+    document.getElementById('newpage').classList.add('hidden')
+    document.getElementById('removed').classList.remove('hidden')
+
+
+
+
+
+})
+
+
+donationPart.addEventListener('click', function () {
+
+    historyPart.classList.remove('bg-lime-400');
+
+    donationPart.classList.add('bg-lime-400')
+
+    document.getElementById('newpage').classList.remove('hidden')
+
+    document.getElementById('removed').classList.add('hidden')
+
+
+
+
+
+})
+
+
+
+
+
+
